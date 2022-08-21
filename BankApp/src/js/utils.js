d6887cb08bc91dd80dda909fbfd195cfd6cf3d8b
acharayaP03 <acharyaP03@gmail.com>
2022-08-21 20:40:11 +1000

    seperate all functions to different file

--- BankApp/src/js/utils.js
+++ BankApp/src/js/utils.js
@@ -9,3 +9,39 @@ export const totalBalance = function (movements, handle) {
 
   handle.textContent = balance + '€';
 }
+
+/**
+ * 
+ * @param {*} account user accont
+ * @param {*} sumIn total sum in
+ * @param {*} sumOut 
+ * @param {*} sumIntrest 
+ */
+export const calDisplaySummary = function (account, sumIn, sumOut, sumIntrest) {
+  const income = account.filter(move => move > 0).reduce((acc, curr) => acc + curr, 0);
+  const expenses = account.filter(move => move < 0).reduce((acc, curr) => acc + curr, 0);
+  const intrest  = account.filter(move => move > 0).map(move => move * 1.2/100).reduce((acc, curr) => acc + curr, 0)
+
+  sumIn.textContent = income + '€';
+  sumOut.textContent = Math.abs(expenses) + '€'
+  sumIntrest.textContent = intrest + '€'
+} 
+
+export const displayMovements = function (movements, container) {
+  // if html already contains html then,empty container
+  container.innerHTML = '';
+
+  movements.forEach((move, i) => {
+    const type = move > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__date">3 days ago</div>
+        <div class="movements__value">${move}</div>
+      </div>
+    `;
+    
+    container.insertAdjacentHTML('afterbegin', html);
+  });
+}
