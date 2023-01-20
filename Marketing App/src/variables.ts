c99ca9346a746a2eb92fb834a470fe258091bd99
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-20 22:57:56 +1100

    loading images with lazyly with Intersectin Observer

--- Marketing App/src/variables.ts
+++ Marketing App/src/variables.ts
@@ -12,4 +12,5 @@ export const tabs = document.querySelectorAll('.operations__tab') as NodeList;
 export const tabsContainer = document.querySelector('.operations__tab-container') as HTMLElement;
 export const tabsContent = document.querySelectorAll('.operations__content') as NodeList
 export const nav = document.querySelector('.nav') as HTMLElement;
-export const allSections = document.querySelectorAll('.section') as NodeList
+export const allSections = document.querySelectorAll('.section') as NodeList;
+export const allImages = document.querySelectorAll('img[data-src]') as NodeList;
