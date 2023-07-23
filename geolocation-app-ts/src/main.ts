1c720f7fdf03f34c74150d68be783235ceecb819
acharayaP03 <acharyaP03@gmail.com>
2023-07-23 17:12:31 +1000

    Converting our app to OOP style with ts

--- geolocation-app-ts/src/main.ts
+++ geolocation-app-ts/src/main.ts
@@ -1,62 +1,7 @@
 'use strict';
 import 'leaflet/dist/leaflet.css';
-// @ts-ignore
-import L from 'leaflet';
-import { htmlElment } from './variables';
-import { renderLeafletMap } from './leaflet';
-let mapEvent: { latlng: { lat: any; lng: any } },
-    map: L.Map | L.LayerGroup<any>;
-if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
-            const { longitude, latitude } = position.coords;
 
-            map = renderLeafletMap(htmlElment.map, [latitude, longitude]);
+import App from './OOP/App';
 
-            map.on('click', function (event: any) {
-                mapEvent = event;
-                htmlElment.form?.classList.remove('hidden');
-                htmlElment?.inputDistance?.focus();
-            });
-        },
-        function () {
-            alert('Could not get your position...');
-        }
-    );
+const app = new App();
 
-htmlElment.form?.addEventListener('submit', function (e: Event) {
-    e.preventDefault();
-    const { lat, lng } = mapEvent.latlng;
-
-    const { inputDistance, inputCadence, inputDuration, inputElevation } =
-        htmlElment;
-
-    inputDistance.value =
-        inputCadence.value =
-        inputDuration.value =
-        inputElevation.value =
-            '';
-
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-            L.popup({
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: 'running-popup',
-            })
-        )
-        .setPopupContent('Working out...')
-        .openPopup();
-});
-
-htmlElment.inputType?.addEventListener('change', function () {
-    htmlElment.inputElevation
-        .closest('.form__row')
-        ?.classList.toggle('form__row--hidden');
-    htmlElment.inputCadence
-        .closest('.form__row')
-        ?.classList.toggle('form__row--hidden');
-});
